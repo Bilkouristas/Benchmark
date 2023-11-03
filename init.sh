959fa1576e57beca7f3f62d7449f7af65206f2a2
Bill Kour <bilkouristas@Basiless-MacBook-Pro.local>
2023-11-03 12:04:45 +0000

    1

---
+++ init.sh
@@ -0,0 +1,10 @@
+!#/usr/bin/zsh
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/Bilkouristas/Benchmark.git
+git push -u origin master
+
+
